a76dc902c6a0e588dd5847d2f9dee3839a5ce5fe
LinColn <86668439+alphabiz-se2@users.noreply.github.com>
2023-01-05 16:55:24 +0800

    prevent to mkdir

--- app.js
+++ app.js
@@ -2,22 +2,32 @@ import express from 'express'
 import Gun from 'gun'
 import path from "path";
 
+console.log('=============================================================================')
+console.log('============================== start ========================================')
+console.log('=============================================================================')
+
 const port = normalizePort(process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765);
 const public_path = path.join(path.resolve('.'), 'public')
-const store_directory = 'data'
+
 console.log('public_path', public_path)
 const app = express();
-// app.use(Gun.serve);
+app.use(Gun.serve);
 app.use(express.static(public_path));
 const server = app.listen(port);
 
-// const gun = Gun({
-//   file: store_directory,
-//   web: server
-// });
-// global.Gun = Gun; /// make global to `node --inspect` - debug only
-// global.gun = gun; /// make global to `node --inspect` - debug only
-// console.log('Server started on port ' + port + ' with /gun');
+const gun = Gun({
+  web: server,
+  rfs: false,
+  // file: store_directory,
+  // s3: {
+  //   key: process.env.AWS_ACCESS_KEY_ID, // AWS Access Key
+  //   secret: process.env.AWS_SECRET_ACCESS_KEY, // AWS Secret Token
+  //   bucket: process.env.AWS_S3_BUCKET // The bucket you want to save into
+  // }
+});
+global.Gun = Gun; /// make global to `node --inspect` - debug only
+global.gun = gun; /// make global to `node --inspect` - debug only
+console.log('Server started on port ' + port + ' with /gun');
 
 function normalizePort(val) {
   const port = parseInt(val, 10);
