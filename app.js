47fba7514ed53acc5f53cc5de135b7d5af7726bf
LinColn <86668439+alphabiz-se2@users.noreply.github.com>
2023-01-05 16:17:56 +0800

    disable Gun features

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ const public_path = path.join(path.resolve('.'), 'public')
 const store_directory = 'data'
 console.log('public_path', public_path)
 const app = express();
-app.use(Gun.serve);
+// app.use(Gun.serve);
 app.use(express.static(public_path));
 const server = app.listen(port);
-const gun = Gun({file: store_directory, web: server});
-global.Gun = Gun; /// make global to `node --inspect` - debug only
-global.gun = gun; /// make global to `node --inspect` - debug only
 
-console.log('Server started on port ' + port + ' with /gun');
+// const gun = Gun({
+//   file: store_directory,
+//   web: server
+// });
+// global.Gun = Gun; /// make global to `node --inspect` - debug only
+// global.gun = gun; /// make global to `node --inspect` - debug only
+// console.log('Server started on port ' + port + ' with /gun');
 
 function normalizePort(val) {
   const port = parseInt(val, 10);
